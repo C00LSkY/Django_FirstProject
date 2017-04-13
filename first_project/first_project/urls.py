0f91d99066beea504c201dc5f1584ebadd5aeca6
C00LSkY <c00lspam1@gmail.com>
2017-04-14 00:13:33 +0300

    подклюяили main_app настроили главную страницу

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from main_app.views import main_view
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', main_view),
 ]
